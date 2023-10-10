b6e37980163855abc16115b90536671e885afaa0
Mary Bruff <marybruff5@gmail.com>
2023-10-09 19:23:35 -0600

    add card function and unskip tests to pass

---
+++ src/card.js
@@ -0,0 +1,13 @@
+const createCard = (id, question, answers, correctAnswer) => {
+    let card = {
+        id: id,
+        question: question,
+        answers: answers,
+        correctAnswer: correctAnswer,
+    }
+    return card
+}
+
+module.exports = {
+    createCard,
+}
