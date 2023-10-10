db3b62caa8fa56dc1a5fdf28b0a9de66c4f3fd77
Mary Bruff <marybruff5@gmail.com>
2023-10-09 19:43:03 -0600

    add guess function and passing tests with before each

--- test/Card-test.js
+++ test/Card-test.js
@@ -1,19 +1,31 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const { createCard } = require('../src/card');
+const { createCard, evaluateGuess } = require('../src/card');
 
 describe('card', function() {
+  let card; 
+
+  beforeEach(function() {
+    card = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+  });
+
   it('should be a function', function() {
     expect(createCard).to.be.a('function');
   });
 
   it('should create a card and its properties', function() {
-    const card = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    
     expect(card.id).to.equal(1);
     expect(card.question).to.equal('What allows you to define a set of related information using key-value pairs?');
     expect(card.answers).to.deep.equal(['object', 'array', 'function']);
     expect(card.correctAnswer).to.equal('object');
-  });  
+  });
+
+  it('should evaluate if answer is correct or incorrect', function () {
+    const correctAnswer = evaluateGuess('Correct!', 'Correct!');
+    const incorrectAnswer = evaluateGuess('Incorrect!', 'Correct!');
+    
+    expect(correctAnswer).to.equal('Correct!');
+    expect(incorrectAnswer).to.equal('Incorrect!');
+  });
 });
